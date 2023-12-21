f45b67bbbe334b341d5eaa046d40836addfbef9c
KSL78 <acc8041@gmail.com>
2023-12-22 02:28:00 +0900

    Update write_ok.php

--- aproj/page/write_ok.php
+++ aproj/page/write_ok.php
@@ -9,15 +9,32 @@ if(isset($_POST['lockpost'])){
 	$lo_post = '0';
 }
 
+if($_FILES['upload_file'] != NULL){ // 12-18 업로드 테스트 추가문구
+    $tmp_name = $_FILES['upload_file']['tmp_name'];
+    $ux_name = $_FILES['upload_file']['name'];
+	$xname = $_POST['name'];
+	$idx1 = $_GET['idx'];
+    $path = "../upload/$xname";
+    if(!file_exists($path)){
+        mkdir($path, 0777, true);
+        chmod($path, 0777);
+        $up = move_uploaded_file($tmp_name, "$path/$ux_name");
+    }else{
+        $up = move_uploaded_file($tmp_name, "$path/$ux_name");
+    }
+	rename("$path/$ux_name","$path/1.jpg");
+}
+/* 12-18 업로드 테스트
 $tmpfile =  $_FILES['b_file']['tmp_name'];
 $o_name = $_FILES['b_file']['name'];
 $filename = iconv("UTF-8", "EUC-KR",$_FILES['b_file']['name']);
 $folder = "../upload/".$filename;
 move_uploaded_file($tmpfile,$folder);
+*/
 
 $mqq = mq("alter table board auto_increment =1"); //auto_increment 값 초기화
 
-$sql= mq("insert into board(name,pw,title,content,date,lock_post,file) values('".$_POST['name']."','".$userpw."','".$_POST['title']."','".$_POST['content']."','".$date."','".$lo_post."','".$o_name."')");
+$sql= mq("insert into board(name,pw,title,content,date,lock_post,file) values('".$_POST['name']."','".$userpw."','".$_POST['title']."','".$_POST['content']."','".$date."','".$lo_post."','".$ux_name."')");
 echo "<script>alert('글쓰기 완료되었습니다.');</script>";
 ?>
 <meta http-equiv="refresh" content="0 url=/aproj/index.php" />
