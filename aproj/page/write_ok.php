cc77b41e4bfa49e8a7c3f70a0e5eec383e664cb0
KSL78 <acc8041@gmail.com>
2023-12-24 01:25:47 +0900

    Update write_ok.php

--- aproj/page/write_ok.php
+++ aproj/page/write_ok.php
@@ -2,39 +2,48 @@
 
 include $_SERVER['DOCUMENT_ROOT']."/aproj/db.php";
 $date = date('Y-m-d');
+$idx1 = $_GET['idx'];
 $userpw = password_hash($_POST['pw'], PASSWORD_DEFAULT);
 if(isset($_POST['lockpost'])){
 	$lo_post = '1';
 }else{
 	$lo_post = '0';
 }
+$xname = $_POST['name'];
 
-if($_FILES['upload_file'] != NULL){ // 12-18 업로드 테스트 추가문구
-    $tmp_name = $_FILES['upload_file']['tmp_name'];
-    $ux_name = $_FILES['upload_file']['name'];
-	$xname = $_POST['name'];
-	$idx1 = $_GET['idx'];
-    $path = "../upload/$xname";
-    if(!file_exists($path)){
-        mkdir($path, 0777, true);
-        chmod($path, 0777);
-        $up = move_uploaded_file($tmp_name, "$path/$ux_name");
-    }else{
-        $up = move_uploaded_file($tmp_name, "$path/$ux_name");
-    }
-	rename("$path/$ux_name","$path/1.jpg");
+$path = "../upload/$xname";
+$cntv = count($_FILES['upload_file']['name']);
+if(!file_exists($path)){
+    mkdir($path, 0777, true);
+    chmod($path, 0777);
 }
-/* 12-18 업로드 테스트
-$tmpfile =  $_FILES['b_file']['tmp_name'];
-$o_name = $_FILES['b_file']['name'];
-$filename = iconv("UTF-8", "EUC-KR",$_FILES['b_file']['name']);
-$folder = "../upload/".$filename;
-move_uploaded_file($tmpfile,$folder);
-*/
 
-$mqq = mq("alter table board auto_increment =1"); //auto_increment 값 초기화
+for ( $i = 0; $i < $cntv; $i++) {
+    if ( isset($_FILES['upload_file']['name'][$i])
+    && $_FILES['upload_file']['size'][$i] > 0 ) {
+        $ux_name = $_FILES['upload_file']['name'][$i];
+        $tmp_name = $_FILES['upload_file']['tmp_name'][$i];
+        $ret =getimagesize($tmp_name, $extinfo);
+        list($image_width, $image_height, $image_type, $image_attr, $image_mime) = $ret;
+        if($image_type == 2)
+        { 
+        // 원하는 작업을 했다.
+            $up = move_uploaded_file($tmp_name, "$path/$ux_name");
+            rename("$path/$ux_name","$path/$i.jpg");
+            $mqq = mq("alter table board auto_increment =1"); //auto_increment 값 초기화
 
-$sql= mq("insert into board(name,pw,title,content,date,lock_post,file) values('".$_POST['name']."','".$userpw."','".$_POST['title']."','".$_POST['content']."','".$date."','".$lo_post."','".$ux_name."')");
-echo "<script>alert('글쓰기 완료되었습니다.');</script>";
+            $sql= mq("insert into board(name,pw,title,content,date,lock_post,file,pcnt) values('".$_POST['name']."','".$userpw."','".$_POST['title']."','".$_POST['content']."','".$date."','".$lo_post."','".$ux_name."','".$cntv."')");
+            echo "<script>alert('jpg사진이 업로드 되었습니다.');</script>";
+    
+    
+        }
+        else{
+        // 오류다.}
+        echo "<script>alert('사진이 아닙니다.');</script>";
+        header("location:/aproj/page/write.php");
+        }
+    }
+}
+
 ?>
 <meta http-equiv="refresh" content="0 url=/aproj/index.php" />
