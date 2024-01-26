40119fd9e79cf75876a3558ff7570d0a0aa934cb
KSL78 <acc8041@gmail.com>
2024-01-26 18:07:43 +0900

    Update write.php

--- aproj/page/write.php
+++ aproj/page/write.php
@@ -1,38 +1,30 @@
 <!doctype html>
 <head>
 <meta charset="UTF-8">
-<title>게시판</title>
+<title>작성</title>
 <link rel="stylesheet" type="text/css" href="/aproj/css/style.css" />
 </head>
 <body>
     <div id="board_write">
-        <h1><a href="/">자유게시판</a></h1>
-        <h4>글을 작성하는 공간입니다.</h4>
+        <h1><a href="/">사진 게시판</a></h1>
             <div id="write_area">
                 <form action="write_ok.php" method="post" enctype="multipart/form-data">
                     <div id="in_title">
-                        <textarea name="title" id="utitle" rows="1" cols="55" placeholder="제목" maxlength="100" required></textarea>
+                        <textarea name="title" id="utitle" rows="1" cols="55" placeholder="title" maxlength="100" required></textarea>
                     </div>
                     <div class="wi_line"></div>
                     <div id="in_name">
-                        <textarea name="name" id="uname" rows="1" cols="55" placeholder="글쓴이" maxlength="100" required></textarea>
+                        <textarea name="name" id="uname" rows="1" cols="55" placeholder="writer" maxlength="100" required></textarea>
                     </div>
                     <div class="wi_line"></div>
                     <div id="in_content">
-                        <textarea name="content" id="ucontent" placeholder="내용" required></textarea>
+                        <textarea name="content" id="ucontent" placeholder="글을 작성하여 주세요(필수X)" ></textarea>
                     </div>
                     <div id="in_pw">
-                        <input type="password" name="pw" id="upw"  placeholder="비밀번호" required />  
+                        <input type="password" name="pw" id="upw"  placeholder="password" required />  
                     </div>
-                    <div id="in_lock">
-                        <input type="checkbox" value="1" name="lockpost" />해당글을 잠급니다.
-                    </div>
-                    <!--<div id="in_file"> 12-18 파일업로드 테스트
-                        <input type="file" value="1" name="b_file" />
-                    </div>-->
                     <div id="in_file">
-                        <!-- multipul추가 -->
-                        <input type="file" name="upload_file[]" id="upload_file"  required multiple>
+                        <input type="file" name="upload_file[]" id="upload_file"  multiple required>
                     </div>
                     <div class="bt_se">
                         <button type="submit">글 작성</button>
