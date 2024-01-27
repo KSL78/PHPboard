3a50c8ff70b2f98d04f23b3dd6074c536fa625b0
KSL78 <acc8041@gmail.com>
2024-01-27 19:17:02 +0900

    Update delete.php

--- aproj/page/delete.php
+++ aproj/page/delete.php
@@ -1,8 +1,8 @@
 <?php
 	include $_SERVER['DOCUMENT_ROOT']."/aproj/db.php";
 	
-	$bno = $_GET['idx'];
-	$sql = mq("delete from board where idx='$bno';");
+	$idxc = $_GET['idx'];
+	$sql = mq("delete from board where idx='$idxc';");
 ?>
 <script type="text/javascript">alert("삭제되었습니다.");</script>
 <meta http-equiv="refresh" content="0 url=/aproj/index.php" />
