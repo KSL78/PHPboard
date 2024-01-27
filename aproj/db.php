aafb63d271c745ce2e96a6b8c85c7188bce8001d
KSL78 <acc8041@gmail.com>
2024-01-27 19:16:35 +0900

    Update db.php

--- aproj/db.php
+++ aproj/db.php
@@ -1,9 +1,9 @@
 <?php
 	header('Content-Type: text/html; charset=utf-8'); // utf-8인코딩
-	$DBa = "localhost";
-	$DBi = "root";
-	$DBp = "8041";
-	$DBn = "pdb";
+	$DBa = "";
+	$DBi = "";
+	$DBp = "";
+	$DBn = "";
 	// DBa=localhost=DB주소(아파치), DBi=DB계정아이디, DBp=DB계정비밀번호, DBn=DB이름
 	$db = new mysqli($DBa,$DBi,$DBp,$DBn); 
 	$db->set_charset("utf8");
